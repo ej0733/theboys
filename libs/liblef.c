59339b48cbff2328c72ef33a1b49616a190b8d2a
Edison Matias <devedisonmatias@gmail.com>
2023-02-13 19:54:58 -0300

    refactor: liblef

--- libs/liblef.c
+++ libs/liblef.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "liblef.h"
 #include <string.h>
+#include "liblef.h"
+
 /* A  LEF é uma lista que armazena eventos ordenados pelo membro tempo
  * do tipo evento_t. Cada evento pode ter um tipo e armazenar em dados
- * diferentes estruturas de dados que permitem descrever cada evento. */
+ * diferentes estruturas de dados que permitem descrever cada evento.
+ */
+
 /*
  * Cria uma LEF vazia
  */
 lef_t *cria_lef()
 {
-  lef_t *l;
-  nodo_lef_t *nodo;
-  nodo = NULL;
-  if (!(l = malloc(sizeof(lef_t))))
+  lef_t *lef;
+
+  if (!(lef = (lef_t *)malloc(sizeof(lef_t))))
     return NULL;
-  l->Primeiro = nodo;
-  return l;
+
+  lef->Primeiro = NULL;
+
+  return lef;
 }
 
 /*
@@ -24,16 +28,23 @@ lef_t *cria_lef()
  */
 lef_t *destroi_lef(lef_t *l)
 {
-  nodo_lef_t *nodo, *nodo2;
+  nodo_lef_t *nodo, *aux;
+
   nodo = l->Primeiro;
+
   while (nodo != NULL)
   {
-    nodo2 = nodo->prox;
+    aux = nodo->prox;
+
     free(nodo->evento);
     free(nodo);
-    nodo = nodo2;
+
+    nodo = aux;
   }
+
   free(l);
+  l = NULL;
+
   return NULL;
 }
 
@@ -45,26 +56,33 @@ lef_t *destroi_lef(lef_t *l)
 int adiciona_inicio_lef(lef_t *l, evento_t *evento)
 {
   nodo_lef_t *nodo;
-  if (!(nodo = malloc(sizeof(nodo_lef_t))))
+
+  if (!(nodo = (nodo_lef_t *)malloc(sizeof(nodo_lef_t))))
     return 0;
-  if (!(nodo->evento = malloc(sizeof(evento_t))))
+
+  if (!(nodo->evento = (evento_t *)malloc(sizeof(evento_t))))
     return 0;
 
   memcpy(nodo->evento, evento, sizeof(evento_t));
+
   nodo->prox = l->Primeiro;
   l->Primeiro = nodo;
+
   return 1;
 }
 
 nodo_lef_t *aux_ordem_lef(nodo_lef_t *nodo, nodo_lef_t *nodo2, nodo_lef_t *nodo3)
 {
-  if ((nodo3 == NULL) || (((nodo3->evento)->tempo) > ((nodo->evento)->tempo)))
+  if (nodo3 == NULL || nodo3->evento->tempo > nodo->evento->tempo)
   {
     nodo->prox = nodo3;
     nodo2->prox = nodo;
+
     return nodo2;
   }
+
   nodo2->prox = aux_ordem_lef(nodo, nodo2->prox, nodo3->prox);
+
   return nodo2;
 }
 
@@ -76,19 +94,24 @@ nodo_lef_t *aux_ordem_lef(nodo_lef_t *nodo, nodo_lef_t *nodo2, nodo_lef_t *nodo3
 int adiciona_ordem_lef(lef_t *l, evento_t *evento)
 {
   nodo_lef_t *nodo, *nodo2, *nodo3;
-  if (!(nodo = malloc(sizeof(nodo_lef_t))))
+
+  if (!(nodo = (nodo_lef_t *)malloc(sizeof(nodo_lef_t))))
     return 0;
-  if (!(nodo->evento = malloc(sizeof(evento_t))))
+
+  if (!(nodo->evento = (evento_t *)malloc(sizeof(evento_t))))
     return 0;
 
   nodo2 = l->Primeiro;
   memcpy(nodo->evento, evento, sizeof(evento_t));
-  if ((nodo2 == NULL) || ((nodo2->evento)->tempo > (nodo->evento)->tempo))
+
+  if (nodo2 == NULL || nodo2->evento->tempo > nodo->evento->tempo)
   {
     nodo->prox = l->Primeiro;
     l->Primeiro = nodo;
+
     return 1;
   }
+
   nodo3 = nodo2->prox;
   l->Primeiro = aux_ordem_lef(nodo, nodo2, nodo3);
 
@@ -104,12 +127,20 @@ evento_t *obtem_primeiro_lef(lef_t *l)
 {
   nodo_lef_t *nodo;
   evento_t *evento;
-  if (!(evento = malloc(sizeof(evento_t))))
+
+  if (!(evento = (evento_t *)malloc(sizeof(evento_t))))
     return 0;
+
   nodo = l->Primeiro;
+
   memcpy(evento, nodo->evento, sizeof(evento_t));
+
   l->Primeiro = nodo->prox;
+
   free(nodo->evento);
   free(nodo);
+
+  nodo = NULL;
+
   return evento;
 }
