551cd85c99b73d4883a06f9a0a456a91449f8bd9
Edison Matias <devedisonmatias@gmail.com>
2023-02-13 19:44:37 -0300

    refactor: libconjunto

--- libs/libconjunto.c
+++ libs/libconjunto.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "libconjunto.h"
+
 /*
  * Cria um conjunto vazio e o retorna, se falhar retorna NULL.
  * max eh o tamanho maximo do conjunto, isto eh, o tamanho maximo do vetor
@@ -11,9 +12,10 @@ conjunto_t *cria_cjt(int max)
 
     conjunto_t *c;
 
-    if (!(c = malloc(sizeof(conjunto_t))))
+    if (!(c = (conjunto_t *)malloc(sizeof(conjunto_t))))
         return NULL;
-    if (!(c->v = malloc(sizeof(int) * (max))))
+
+    if (!(c->v = (int *)malloc(sizeof(int) * (max))))
         return NULL;
 
     c->max = max;
@@ -29,6 +31,9 @@ conjunto_t *destroi_cjt(conjunto_t *c)
 {
     free(c->v);
     free(c);
+
+    c = NULL;
+
     return NULL;
 }
 
@@ -37,9 +42,7 @@ conjunto_t *destroi_cjt(conjunto_t *c)
  */
 int vazio_cjt(conjunto_t *c)
 {
-    if (c->card == 0)
-        return 1;
-    return 0;
+    return (cardinalidade_cjt(c) == 0);
 }
 
 /*
@@ -56,27 +59,28 @@ int cardinalidade_cjt(conjunto_t *c)
  */
 int busca_cjt(conjunto_t *c, int elemento)
 {
-    int a = 0, b = (c->card - 1), i;
+    int inicio, fim, i;
+
+    inicio = 0;
+    fim = (cardinalidade_cjt(c) - 1);
+
     do
     {
-        i = (a + b) / 2;
+        i = (inicio + fim) / 2;
 
-        if (*(c->v + i) > elemento)
-        {
-            b = i - 1;
-        }
+        if (c->v[i] > elemento)
+            fim = i - 1;
         else
-        {
-            a = i + 1;
-        }
-    } while (a <= b);
+            inicio = i + 1;
+    } while (inicio <= fim);
 
-    if (a > 0)
+    if (inicio > 0)
     {
-        if (*(c->v + a - 1) == elemento)
-            return a - 1;
+        if (c->v[inicio - 1] == elemento)
+            return inicio - 1;
     }
-    return a;
+
+    return inicio;
 }
 
 /*
@@ -86,29 +90,31 @@ int busca_cjt(conjunto_t *c, int elemento)
  */
 int insere_cjt(conjunto_t *c, int elemento)
 {
-    int posi, i;
+    int indice, i;
 
-    if ((c->card) >= (c->max))
+    if (cardinalidade_cjt(c) >= (c->max))
         return 0;
 
     if (vazio_cjt(c))
     {
         *(c->v) = elemento;
         c->card = 1;
+
         return 1;
     }
 
-    posi = busca_cjt(c, elemento);
+    indice = busca_cjt(c, elemento);
 
-    if (posi < c->card)
+    if (indice < cardinalidade_cjt(c))
     {
-        if ((*(c->v + posi)) == elemento)
+        if (c->v[indice] == elemento)
             return 1;
-        for (i = (c->card); i > posi; i--)
-            *(c->v + i) = *(c->v + i - 1);
+
+        for (i = cardinalidade_cjt(c); i > indice; i--)
+            c->v[i] = c->v[i - 1];
     }
 
-    *(c->v + posi) = elemento;
+    c->v[indice] = elemento;
     c->card = c->card + 1;
 
     return 1;
@@ -120,24 +126,26 @@ int insere_cjt(conjunto_t *c, int elemento)
  */
 int retira_cjt(conjunto_t *c, int elemento)
 {
-    int i, posi;
+    int i, indice;
 
-    if (c->card == 0)
+    if (cardinalidade_cjt(c) == 0)
         return 0;
 
-    posi = busca_cjt(c, elemento);
+    indice = busca_cjt(c, elemento);
 
-    if (posi < c->card)
+    if (indice < c->card)
     {
-        if ((*(c->v + posi)) != elemento)
+        if (c->v[indice] != elemento)
             return 0;
 
-        for (i = posi; i <= (c->card - 2); i++)
-            *(c->v + i) = *(c->v + i + 1);
-        c->card = c->card - 1;
+        for (i = indice; i <= (c->card - 2); i++)
+            c->v[i] = c->v[i + 1];
+
+        c->card--;
 
         return 1;
     }
+
     return 0;
 }
 
@@ -146,11 +154,11 @@ int retira_cjt(conjunto_t *c, int elemento)
  */
 int pertence_cjt(conjunto_t *c, int elemento)
 {
-    int posi;
+    int indice;
 
-    posi = busca_cjt(c, elemento);
+    indice = busca_cjt(c, elemento);
 
-    if ((posi >= c->card) || ((*(c->v + posi)) != elemento))
+    if (indice >= cardinalidade_cjt(c) || c->v[indice] != elemento)
         return 0;
 
     return 1;
@@ -162,13 +170,14 @@ int pertence_cjt(conjunto_t *c, int elemento)
 int contido_cjt(conjunto_t *c1, conjunto_t *c2)
 {
     int i;
-    if (c1->card > c2->card)
+
+    if (cardinalidade_cjt(c1) > cardinalidade_cjt(c2))
         return 0;
-    for (i = 0; i < c1->card; i++)
-    {
-        if (!(pertence_cjt(c2, *(c1->v + i))))
+
+    for (i = 0; i < cardinalidade_cjt(c1); i++)
+        if (!(pertence_cjt(c2, c1->v[i])))
             return 0;
-    }
+
     return 1;
 }
 
@@ -178,13 +187,14 @@ int contido_cjt(conjunto_t *c1, conjunto_t *c2)
 int sao_iguais_cjt(conjunto_t *c1, conjunto_t *c2)
 {
     int i;
-    if (c1->card != c2->card)
+
+    if (cardinalidade_cjt(c1) != cardinalidade_cjt(c2))
         return 0;
+
     for (i = 0; i < c1->card; i++)
-    {
-        if (*(c1->v + i) != *(c2->v + i))
+        if (c1->v[i] != c2->v[i])
             return 0;
-    }
+
     return 1;
 }
 
@@ -194,12 +204,15 @@ int sao_iguais_cjt(conjunto_t *c1, conjunto_t *c2)
  */
 conjunto_t *diferenca_cjt(conjunto_t *c1, conjunto_t *c2)
 {
-    conjunto_t *dif;
     int i;
-    dif = copia_cjt(c1);
-    for (i = 0; i <= (c2->card - 1); i++)
-        retira_cjt(dif, (*(c2->v + i)));
-    return dif;
+    conjunto_t *diferenca;
+
+    diferenca = copia_cjt(c1);
+
+    for (i = 0; i < cardinalidade_cjt(c2); i++)
+        retira_cjt(diferenca, c2->v[i]);
+
+    return diferenca;
 }
 
 /*
@@ -208,17 +221,18 @@ conjunto_t *diferenca_cjt(conjunto_t *c1, conjunto_t *c2)
  */
 conjunto_t *interseccao_cjt(conjunto_t *c1, conjunto_t *c2)
 {
-    conjunto_t *inter;
     int i;
+    conjunto_t *interseccao;
 
-    if (!(inter = cria_cjt(c1->card + c2->card)))
+    if (!(interseccao = cria_cjt(cardinalidade_cjt(c1) + cardinalidade_cjt(c2))))
         return NULL;
-    for (i = 0; i < c2->card; i++)
+
+    for (i = 0; i < cardinalidade_cjt(c2); i++)
     {
-        if (pertence_cjt(c1, *(c2->v + i)))
-            insere_cjt(inter, *(c2->v + i));
+        if (pertence_cjt(c1, c2->v[i]))
+            insere_cjt(interseccao, c2->v[i]);
     }
-    return inter;
+    return interseccao;
 }
 
 /*
@@ -227,21 +241,26 @@ conjunto_t *interseccao_cjt(conjunto_t *c1, conjunto_t *c2)
  */
 conjunto_t *uniao_cjt(conjunto_t *c1, conjunto_t *c2)
 {
-    conjunto_t *uni;
     int i;
-    uni = copia_cjt(c1);
-    if (uni == NULL)
+    conjunto_t *uniao;
+
+    uniao = copia_cjt(c1);
+
+    if (uniao == NULL)
     {
-        free(uni);
-        uni = copia_cjt(c2);
-        if (uni == NULL)
+        free(uniao);
+        uniao = copia_cjt(c2);
+
+        if (uniao == NULL)
             return NULL;
-        return uni;
+
+        return uniao;
     }
 
-    for (i = 0; i < (c2->card); i++)
-        insere_cjt(uni, *(c2->v + i));
-    return uni;
+    for (i = 0; i < cardinalidade_cjt(c2); i++)
+        insere_cjt(uniao, c2->v[i]);
+
+    return uniao;
 }
 
 /*
@@ -249,15 +268,16 @@ conjunto_t *uniao_cjt(conjunto_t *c1, conjunto_t *c2)
  */
 conjunto_t *copia_cjt(conjunto_t *c)
 {
-    conjunto_t *cop;
     int i;
-    if ((cop = cria_cjt(c->max)) == (NULL))
+    conjunto_t *copia;
+
+    if ((copia = cria_cjt(c->max)) == (NULL))
         return NULL;
-    for (i = 0; i <= (c->card - 1); i++)
-    {
-        insere_cjt(cop, *(c->v + i));
-    }
-    return cop;
+
+    for (i = 0; i < cardinalidade_cjt(c); i++)
+        insere_cjt(copia, c->v[i]);
+
+    return copia;
 }
 
 /*
@@ -268,20 +288,27 @@ conjunto_t *copia_cjt(conjunto_t *c)
  */
 conjunto_t *cria_subcjt_cjt(conjunto_t *c, int n)
 {
-    conjunto_t *sub;
-    int posi_aleat, max = c->card - 1;
-    if (n >= c->card)
+    int posicao_aleatoria, max;
+    conjunto_t *subcjt;
+
+    max = cardinalidade_cjt(c) - 1;
+
+    if (n >= cardinalidade_cjt(c))
     {
-        sub = copia_cjt(c);
-        return sub;
+        subcjt = copia_cjt(c);
+
+        return subcjt;
     }
-    sub = cria_cjt(n);
-    while (sub->card < n)
+
+    subcjt = cria_cjt(n);
+    
+    while (cardinalidade_cjt(subcjt) < n)
     {
-        posi_aleat = 0 + rand() % (max);
-        insere_cjt(sub, *(c->v + posi_aleat));
+        posicao_aleatoria = 0 + rand() % (max);
+        insere_cjt(subcjt, c->v[posicao_aleatoria]);
     }
-    return sub;
+    
+    return subcjt;
 }
 
 /*
@@ -294,17 +321,20 @@ conjunto_t *cria_subcjt_cjt(conjunto_t *c, int n)
 void imprime_cjt(conjunto_t *c)
 {
     int i;
-    if (c->card == 0)
+
+    if (cardinalidade_cjt(c) == 0)
         printf("conjunto vazio");
     else
     {
-        for (i = 0; i < (c->card); i++)
+        for (i = 0; i < cardinalidade_cjt(c); i++)
         {
-            printf("%d", *(c->v + i));
-            if (i < (c->card - 1))
+            printf("%d", c->v[i]);
+
+            if (i < cardinalidade_cjt(c) - 1)
                 printf(" ");
         }
     }
+
     printf("\n");
 }
 
@@ -331,10 +361,12 @@ void inicia_iterador_cjt(conjunto_t *c)
  */
 int incrementa_iterador_cjt(conjunto_t *c, int *ret_iterador)
 {
-    if (c->ptr > c->card - 1)
+    if (c->ptr > cardinalidade_cjt(c) - 1)
         return 0;
-    *ret_iterador = *(c->v + (c->ptr));
-    c->ptr = c->ptr + 1;
+
+    *ret_iterador = c->v[c->ptr];
+    c->ptr++;
+
     return 1;
 }
 
@@ -346,6 +378,8 @@ int incrementa_iterador_cjt(conjunto_t *c, int *ret_iterador)
 int retira_um_elemento_cjt(conjunto_t *c)
 {
     int elemento = *(c->v);
+
     retira_cjt(c, elemento);
+    
     return elemento;
 }
